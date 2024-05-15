813761954f69be738c2ffbc882adab5b7f02b4bb
Animesh Sharma <animesh7137@gmail.com>
2024-05-15 13:19:01 +0530

    adding modular functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from cal_fun import do_addition,do_multiplication,do_substraction
+from cal_fun import do_addition,do_multiplication,do_substraction,do_perceentile
 from devied import do_devied
 def main():
-    print("press 1 for add 2 for substraction 3 for multiplication and 4 for devied")
+    print("press 1 for add 2 for substraction 3 for multiplication and 4 for devied 5 modulas")
 if __name__ == "__main__":
     main()
 user_inpt=input("select the function")
@@ -22,6 +22,10 @@ elif user_inpt=="3":
 
 elif user_inpt=="4":
     result=do_devied(a,b)
+    print("result=",result)
+
+elif user_inpt=="5":
+    result=do_perceentile(a,b)
     print("result=",result)
 else:
     print("you enter wrong number")
--- cal_fun.py
+++ cal_fun.py
@@ -6,3 +6,6 @@ def do_substraction(a:int,b:int):
 
 def do_multiplication(a:int,b:int):
    return a * b   
+ 
+def do_perceentile(a:int,b:int):
+    return a % b
