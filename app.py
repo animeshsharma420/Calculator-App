15d537de79bf5c8f12c2880c83eb76c77308214a
Animesh Sharma <animesh7137@gmail.com>
2024-05-15 12:33:31 +0530

    Revert "adding devied functionality"
    This reverts commit 86e065811eaa3dd74a828714cb58227a322b6d09.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from cal_fun import do_addition,do_multiplication,do_substraction
-from devied import do_devied
 def main():
-    print("press 1 for add 2 for substraction 3 for multiplication and 4 for devied")
+    print("press 1 for add 2 for substraction and 3 for multiplication")
 if __name__ == "__main__":
     main()
 user_inpt=input("select the function")
@@ -18,10 +17,6 @@ elif user_inpt=="2":
 
 elif user_inpt=="3":
     result=do_multiplication(a,b)
-    print("result=",result)
-
-elif user_inpt=="4":
-    result=do_devied(a,b)
     print("result=",result)
 else:
     print("you enter wrong number")
